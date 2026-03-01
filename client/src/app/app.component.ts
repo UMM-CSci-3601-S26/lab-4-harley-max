27a070fb67b0b44d90aacfe5cc78b18e185c6aab
Gabriel Nelson <nel13776@morris.umn.edu>
2026-03-01 15:26:18 -0600

    Add inventory table. Delete type filter.
    Add some dummy data to have table appear on web page. Remove unused imports. Add table display with ability to sort by ascend and descend for each column. Remove unused type input box.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -12,6 +12,7 @@ import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
   styleUrls: ['./app.component.scss'],
   imports: [MatSidenavModule, MatToolbarModule, MatListModule, RouterLink, RouterLinkActive, MatIconModule, MatButtonModule, RouterOutlet]
 })
+
 export class AppComponent {
   title = 'CSCI 3601 Iteration Template';
 }
--- client/src/app/inventory/inventory.component.ts
+++ client/src/app/inventory/inventory.component.ts
@@ -1,34 +1,56 @@
-import { Component} from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { LiveAnnouncer } from '@angular/cdk/a11y';
+import { AfterViewInit, Component, ViewChild, inject} from '@angular/core';
+import { MatTable, MatTableModule, MatTableDataSource } from '@angular/material/table';
 import { MatCardModule } from '@angular/material/card';
-import { MatOptionModule } from '@angular/material/core';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
-import { MatListModule } from '@angular/material/list';
-import { MatSelectModule } from '@angular/material/select';
-//import { MatSnackBar } from '@angular/material/snack-bar';
-import { MatTooltipModule } from '@angular/material/tooltip';
-//import { RouterLink } from '@angular/router';
-//import { catchError, combineLatest, of, switchMap, tap } from 'rxjs';
-//import { toObservable, toSignal } from '@angular/core/rxjs-interop';
+import { MatSort, Sort, MatSortModule } from '@angular/material/sort';
+
+// Dummy data just to see what the table is going to look like, will need to be removed in the future for the actual inventory data
+export interface dummyInventoryData {
+  item: string,
+  description: string,
+  brand: string,
+  color:string,
+  size: string,
+  type: string,
+  material:string,
+  count: number,
+  quantity: number,
+  notes: string
+}
+
+const DUMMY_DATA: dummyInventoryData[] = [
+  {item: 'pencil', description: 'yellow pencil', brand: 'up&up', color: 'yellow', size: 'regular', type: '#2', material: 'N/A', count: 10, quantity: 2, notes: 'N/A'},
+  {item: 'folder', description: 'green plastic pocket folder', brand: 'five star', color: 'green', size: 'regular', type: 'pocket', material: 'plastic', count: 1, quantity: 20, notes: 'N/A'},
+];
 
 @Component({
   selector: 'app-inventory',
   imports: [
-    MatCardModule,
-    MatFormFieldModule,
-    MatInputModule,
-    FormsModule,
-    MatSelectModule,
-    MatOptionModule,
-    MatListModule,
-    MatTooltipModule,
-    MatIconModule,
+   MatTableModule,
+   MatCardModule,
+   MatSortModule
   ],
   templateUrl: './inventory.component.html',
   styleUrl: './inventory.component.scss',
 })
-export class InventoryComponent {
 
+export class InventoryComponent {
+  private _liveAnnouncer = inject(LiveAnnouncer);
+
+  displayedColumns: string[] = ['item', 'description', 'brand', 'color', 'size', 'type', 'material', 'quantity', 'count', 'notes'];
+  dataSource = new MatTableDataSource(DUMMY_DATA);
+
+   @ViewChild(MatSort) sort!: MatSort;
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+  }
+
+  announceSortChange(sortState: Sort) {
+    if (sortState.direction) {
+      this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
+    } else {
+      this._liveAnnouncer.announce('Sorting cleared');
+    }
+  }
 }
