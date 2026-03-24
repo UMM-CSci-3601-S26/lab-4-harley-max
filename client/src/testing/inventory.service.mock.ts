ed7d4e1227da6008f6b88a0b79ca7c977dc5df17
Harley Hannahs <hanna537@morris.umn.edu>
2026-03-24 00:20:56 -0500

    Add options for drop downs in mock
    Add arrays of drop down options to inventory.service.mock.ts so that existing karma tests will pass

--- client/src/testing/inventory.service.mock.ts
+++ client/src/testing/inventory.service.mock.ts
@@ -48,6 +48,56 @@ export class MockInventoryService implements Pick<InventoryService, 'getInventor
     }
   ];
 
+  itemOptions = [
+    { value: 'pencil', label: 'Pencils' },
+    { value: 'colored pencil', label: 'Colored Pencils' },
+    { value: 'sharpener', label: 'Sharpeners' },
+    { value: 'marker', label: 'Markers' },
+    { value: 'highlighter', label: 'Highlighters' },
+    { value: 'crayon', label: 'Crayons' },
+    { value: 'pen', label: 'Pens' },
+    { value: 'eraser', label: 'Erasers' },
+    { value: 'folder', label: 'Folders' },
+    { value: 'binder', label: 'Binders' },
+    { value: 'notebook', label: 'Notebooks' },
+    { value: 'glue', label: 'Glue' },
+    { value: 'ruler', label: 'Rulers' },
+    { value: 'scissors', label: 'Scissors' },
+    { value: 'headphones', label: 'Headphones' },
+    { value: 'backpack', label: 'Backpacks' },
+    { value: 'box', label: 'Boxes' },
+    { value: 'calculator', label: 'Calculators' },
+    { value: 'tissue', label: 'Tissues' },
+    { value: 'water bottle', label: 'Water Bottle' },
+    { value: 'wipe', label: 'Wipes' },
+    { value: 'other', label: 'Other' }
+  ];
+
+  brandOptions = [
+    { value: 'Kleenex', label: 'Kleenex' },
+    { value: 'Sharpie', label: 'Sharpie' },
+    { value: 'Texas Instruments', label: 'Texas Instruments' },
+    { value: 'Ticonderoga', label: 'Ticonderoga' },
+    { value: 'Crayola', label: 'Crayola' },
+    { value: 'Clorox', label: 'Clorox' },
+    { value: 'Expo', label: 'Expo' },
+    { value: 'Elmer', label: 'Elmers' },
+    { value: 'Lysol', label: 'Lysol' },
+    { value: 'Puffs', label: 'Puffs' },
+    { value: 'other', label: 'Other' }
+  ];
+
+  colorOptions = [
+    { value: 'red', label: 'Red' },
+    { value: 'blue', label: 'Blue' },
+    { value: 'yellow', label: 'Yellow' },
+    { value: 'orange', label: 'Orange' },
+    { value: 'green', label: 'Green' },
+    { value: 'purple', label: 'Purple' },
+    { value: 'black', label: 'Black' },
+    { value: 'other', label: 'Other' }
+  ];
+
   /* eslint-disable @typescript-eslint/no-unused-vars */
   getInventory(_filters: { item?: string, brand?: string, color?: string, size?: string, type?: string, material?: string }): Observable<Inventory[]> {
     return of(MockInventoryService.testInventory);
